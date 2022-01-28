a912ad23ef1d06f3f91ccd81328781b7d6c6f68a
PhilosopherProgrammer <nekarakube@gmail.com>
2022-01-28 20:26:59 +0900

    Add shuffling string

--- result.js
+++ result.js
@@ -4,14 +4,27 @@ window.onload = () => {
 
     orgKor.innerText = localStorage.getItem('orgKor');
     const en = localStorage.getItem('orgEn');
-    en.trim().split(" ").forEach((val, idx) => {
+
+    shuffle(en.trim().split(" ")).forEach((str) => {
         const sp = document.createElement("li");
-        let str = '';
-        for (let i = 0; i < val.length; i++) {
-            str += '__';
-        }
-        if (idx === en.split(" ").length - 1) str += '.';
         sp.innerText = str;
         orgEn.appendChild(sp);
     });
 };
+
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
